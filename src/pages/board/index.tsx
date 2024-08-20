9d51e66c94e5a5e427fcfb7575989d4bc49ee840
Quliyeva1o <renaquliyeva646@gmail.com>
2024-08-20 14:45:26 +0400

    .

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -3,17 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchTodos, updateTodo } from "../../store/slices/todoSlice";
 import { AppDispatch, RootState } from "../../store/store";
 import styles from "./index.module.scss";
-import { Button, Flex } from "antd";
+import { Button, Flex, Spin } from "antd";
 import { Link } from "react-router-dom";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
-import { PieChart } from '@mui/x-charts';
+import { PieChart } from "@mui/x-charts";
 import { Droppable } from "./dragDrop/dropable";
 import { Draggable } from "./dragDrop/draggable";
 
 const Board: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
-  const todos = useSelector((state: RootState) => state.todos.todos);
-
+  const {todos, loading} = useSelector((state: RootState) => state.todos);
+  
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
@@ -21,25 +21,30 @@ const Board: React.FC = () => {
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
     if (over) {
-      const isCompleted = over.id === 'completed';
-      const draggedTodo = todos.find(todo => todo.id === active.id);
+      const isCompleted = over.id === "completed";
+      const draggedTodo = todos.find((todo) => todo.id === active.id);
       if (draggedTodo && draggedTodo.completed !== isCompleted) {
-        dispatch(updateTodo({
-          id: draggedTodo.id,
-          updatedTodo: { ...draggedTodo, completed: isCompleted },
-        }));
+        dispatch(
+          updateTodo({
+            id: draggedTodo.id,
+            updatedTodo: { ...draggedTodo, completed: isCompleted },
+          })
+        );
       }
     }
   };
 
-  const completedTodos = todos.filter(todo => todo.completed);
-  const notCompletedTodos = todos.filter(todo => !todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const notCompletedTodos = todos.filter((todo) => !todo.completed);
 
   return (
     <div className={styles.board}>
       <Link to="/" className={styles.homeBtn}>
         <Button type="primary">Home</Button>
       </Link>
+      {
+        loading && <Spin size="large" fullscreen={true} />
+      }
       <PieChart
         series={[
           {
@@ -48,13 +53,13 @@ const Board: React.FC = () => {
                 id: 0,
                 value: completedTodos.length,
                 label: "Completed",
-                color: "green"
+                color: "green",
               },
               {
                 id: 1,
                 value: notCompletedTodos.length,
                 label: "Not Completed",
-                color: "rgb(249, 51, 51)"
+                color: "rgb(249, 51, 51)",
               },
             ],
           },
@@ -67,7 +72,7 @@ const Board: React.FC = () => {
           <Droppable id="completed">
             <div className={`${styles.column} ${styles.completed}`}>
               <h2 className={styles.columnTitle}>Completed</h2>
-              {completedTodos.map(todo => (
+              {completedTodos.map((todo) => (
                 <Draggable key={todo.id.toString()} id={todo.id.toString()}>
                   <div className={styles.todoItem}>
                     <p>{todo.title}</p>
@@ -79,7 +84,7 @@ const Board: React.FC = () => {
           <Droppable id="notCompleted">
             <div className={`${styles.column} ${styles.notComp}`}>
               <h2 className={styles.columnTitle}>Not Completed</h2>
-              {notCompletedTodos.map(todo => (
+              {notCompletedTodos.map((todo) => (
                 <Draggable key={todo.id.toString()} id={todo.id.toString()}>
                   <div className={styles.todoItem}>
                     <p>{todo.title}</p>
@@ -93,6 +98,5 @@ const Board: React.FC = () => {
     </div>
   );
 };
-
 
 export default Board;
